41425f0dc69dfe030b9481d8db2ff07ccdb9954b
Taufik Nurhidayat <fiik346@gmail.com>
2025-03-10 16:27:03 +0700

    save loadtest

--- src/pages/api/requests/post_run.rs
+++ src/pages/api/requests/post_run.rs
@@ -4,6 +4,8 @@ use diesel::prelude::*;
 use std::fs::{File, OpenOptions, create_dir_all};
 use std::io::Write;
 
+use crate::models::load_test::NewLoadTest;
+use crate::schema::load_tests;
 use crate::services::reqwest::send_request;
 use crate::{
     app_state::AppState,
@@ -25,15 +27,28 @@ pub async fn run(state: web::Data<AppState>, id: web::Path<i32>) -> impl Respond
         .get_result(conn)
         .unwrap();
 
-    let req = send_request(&req, &coll, None).await.unwrap();
-    let text = req.text().await.unwrap();
+    let reqw_req = send_request(&req, &coll, None).await.unwrap();
+    let text = reqw_req.text().await.unwrap();
     let log_dir = "./static/log";
     create_dir_all(log_dir).unwrap();
 
     let timestamp = Utc::now().format("%Y%m%d_%H%M%S");
-    let log_path = format!("{}/log_{}.txt", log_dir, timestamp);
+    let file_name = format!("log_{}.txt", timestamp);
+    let log_path = format!("{}/{}", log_dir, file_name);
     let mut file = File::create(&log_path).unwrap();
     file.write(text.as_bytes()).unwrap();
+
+    let new_loadtest = NewLoadTest {
+        name: req.name.clone(),
+        source_type: "Request".to_string(),
+        source_id: req.id,
+        log_path: format!("/log/{}", file_name),
+    };
+
+    let _ = diesel::insert_into(load_tests::table)
+        .values(&new_loadtest)
+        .execute(conn)
+        .unwrap();
 
     println!("{}", text);
     HttpResponse::Ok().body("Ok")
