44b055d24f85ab663f7b36853107da14fdc3547f
Taufik Nurhidayat <fiik346@gmail.com>
2025-03-13 09:01:35 +0700

    refactor: new loadtest and request form

--- src/pages/collections/mod.rs
+++ src/pages/collections/mod.rs
@@ -2,9 +2,11 @@ use actix_web::{Scope, web};
 
 mod collection_detail;
 mod loadtest;
+mod new_request;
 
 pub fn collections_scope() -> Scope {
     web::scope("/collections")
         .route("/{id}", web::get().to(collection_detail::collection_detail))
         .route("/{id}/loadtest", web::get().to(loadtest::new_load_test))
+        .route("/{id}/request", web::get().to(new_request::new_load_test))
 }
---
+++ src/pages/collections/new_request.rs
@@ -0,0 +1,38 @@
+use actix_web::{HttpResponse, Responder, web};
+use serde::{Deserialize, Serialize};
+
+use crate::{
+    app_state::AppState,
+    services::{
+        get_collection::{get_collection_headers, get_main_collections, get_single_collection},
+        get_request::{get_collection_requests, get_request_headers},
+    },
+};
+
+pub async fn new_load_test(state: web::Data<AppState>, id: web::Path<i32>) -> impl Responder {
+    let conn = &mut state.pool.get().unwrap();
+    let mut ctx = tera::Context::new();
+    let collection_id = id.into_inner();
+
+    if let Ok(hdrs) = get_collection_headers(conn, collection_id).await {
+        ctx.insert("default_headers", &hdrs);
+    }
+
+    let collections = get_main_collections(conn).await.unwrap();
+    ctx.insert("collections", &collections);
+
+    if let Ok(collection) = get_single_collection(conn, collection_id).await {
+        ctx.insert("collection", &collection);
+        ctx.insert("COLLECTION_ID", &collection_id);
+
+        let requests = get_collection_requests(conn, collection_id).await.unwrap();
+        ctx.insert("requests", &requests);
+
+        let rendered = state
+            .tera
+            .render("pages/collection/new_request.html", &ctx)
+            .unwrap();
+        return HttpResponse::Ok().body(rendered);
+    };
+    HttpResponse::NotFound().body("404")
+}
