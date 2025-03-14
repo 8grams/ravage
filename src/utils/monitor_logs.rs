7e4b4420a46581c74ae75ae39e1e7700f1eb7dba
Taufik Nurhidayat <fiik346@gmail.com>
2025-03-14 12:58:23 +0700

    add: read logs functions

--- src/utils/monitor_logs.rs
+++ src/utils/monitor_logs.rs
@@ -1,8 +1,16 @@
+use std::io::{Error, ErrorKind};
+use std::time::Duration;
+
 use crate::app_state::AppState;
 use crate::app_state::LogChannel;
-use tokio::sync::broadcast;
 
 use actix_web::web;
+
+use tokio::fs::File;
+use tokio::io::{AsyncBufReadExt, BufReader};
+use tokio::sync::broadcast;
+use tokio::sync::broadcast::Sender;
+use tokio::time::sleep;
 
 pub async fn get_or_create_channel(state: &web::Data<AppState>, id: i32) -> LogChannel {
     let mut channels = state.log_channels.lock().await;
@@ -14,4 +22,30 @@ pub async fn get_or_create_channel(state: &web::Data<AppState>, id: i32) -> LogC
             sender
         })
         .clone()
+}
+
+pub async fn stream_log_file(
+    sender: Sender<String>,
+    log_path: impl AsRef<std::path::Path>,
+) -> Result<(), Error> {
+    if !log_path.as_ref().exists() {
+        return Err(Error::new(
+            std::io::ErrorKind::NotFound,
+            "Log file not found",
+        ));
+    }
+    tokio::spawn(async move {
+        let mut position: u64 = 0;
+
+        loop {
+            let file = match File::open(log_path).await {
+                Ok(file) => file,
+                Err(_) => {
+                    sleep(Duration::from_secs(5)).await;
+                    continue;
+                }
+            };
+        }
+    });
+    Ok(())
 }
