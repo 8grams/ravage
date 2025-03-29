da09aa7a1c911129355a430d87a168fc373f7535
Glend Maatita <glend.maatita@gmail.com>
2025-03-30 05:59:48 +0700

    replace nginx with caddy

--- start.sh
+++ start.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 # Set default values for environment variables if not set
-export APP_URL=${APP_URL:-"80:"}
+export APP_URL=${APP_URL:-":80"}
 
 # Process Caddyfile with environment variables
 envsubst < /etc/caddy/Caddyfile > /etc/caddy/Caddyfile.tmp
 mv /etc/caddy/Caddyfile.tmp /etc/caddy/Caddyfile
 
 # Start supervisor in the background
-/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf &
-
-# Start Caddy in the foreground
-exec caddy run --config /etc/caddy/Caddyfile
+/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf &x
