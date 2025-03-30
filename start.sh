2608cde6bb5fee8dd4c31270bfd14ac67ef2685f
Glend Maatita <glend.maatita@gmail.com>
2025-03-30 07:04:59 +0700

    fix migrations

--- start.sh
+++ start.sh
@@ -7,5 +7,8 @@ export APP_URL=${APP_URL:-":80"}
 envsubst < /etc/caddy/Caddyfile > /etc/caddy/Caddyfile.tmp
 mv /etc/caddy/Caddyfile.tmp /etc/caddy/Caddyfile
 
+# run migrations
+/root/.cargo/bin/diesel migration run
+
 # Start supervisor in the background
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
