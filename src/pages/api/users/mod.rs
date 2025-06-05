44a8d914c6ed9331539c89fbd28b15194f3eb952
Taufik Nurhidayat <fiik346@gmail.com>
2025-06-05 11:39:53 +0700

    Enable user update and role based access control
    The update user API endpoint now uses POST instead of PATCH and
    allows updating all user fields (name, email, role).
    
    RBAC is added to the collection and request options.

--- src/pages/api/users/mod.rs
+++ src/pages/api/users/mod.rs
@@ -2,14 +2,14 @@ use actix_web::{Scope, web};
 
 mod delete_user;
 mod get_user_modal;
-mod patch_update_role;
 mod post_new_user;
+mod post_update_user;
 
 pub fn users_api_scope() -> Scope {
     web::scope("/users")
         .route("", web::post().to(post_new_user::new_user))
         .route("/new", web::get().to(get_user_modal::new_user_modal))
-        .route("/{id}", web::patch().to(patch_update_role::update_role))
+        .route("/{id}", web::post().to(post_update_user::update_user))
         .route("/{id}", web::delete().to(delete_user::delete_user))
         .route("/{id}", web::get().to(get_user_modal::edit_user_modal))
 }
--- src/pages/api/users/post_update_user.rs
+++ src/pages/api/users/post_update_user.rs
@@ -1,25 +1,24 @@
-use crate::{app_state::AppState, schema::users, utils};
+use crate::{app_state::AppState, models::user::NewUser, schema::users, utils};
 use actix_web::{HttpResponse, Responder, web};
 use diesel::RunQueryDsl;
 use diesel::prelude::*;
-use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize)]
-pub struct RoleFormData {
-    pub role: String,
-}
-
-pub async fn update_role(
-    form_data: web::Form<RoleFormData>,
+pub async fn update_user(
     path: web::Path<i32>,
+    form: web::Form<NewUser>,
     state: web::Data<AppState>,
     session: actix_session::Session,
 ) -> impl Responder {
     let conn = &mut state.pool.get().unwrap();
     let session_json = utils::session::get_session_json(&session).await;
+    let form_data = form.into_inner();
     if session_json.is_some_and(|t| t.role == "admin") {
         let _ = diesel::update(users::table)
-            .set(users::role.eq(&form_data.role))
+            .set((
+                users::name.eq(form_data.name),
+                users::email.eq(form_data.email),
+                users::role.eq(form_data.role),
+            ))
             .filter(users::id.eq(path.into_inner()))
             .execute(conn);
     }
